855db9fdacc68ef3b76223883b5860472fc96ea8
Danial Manavi <dmanavi@live.com>
2020-10-04 12:22:44 +0330

    Change spaces into tabs.

--- src/service/logger.ts
+++ src/service/logger.ts
@@ -10,40 +10,40 @@ import { Logger as ILogger, LoggerConfig, } from '../type/logger';
 
 export const create = (config?: LoggerConfig): ILogger => {
 
-    if (!config) { throw new MissingArgumentError('config'); }
+	if (!config) { throw new MissingArgumentError('config'); }
 
-    const logger = createLogger({
-        transports: [],
-        exitOnError: true
-    });
+	const logger = createLogger({
+		transports: [],
+		exitOnError: true
+	});
 
-    if (config.environment === 'dev') {
+	if (config.environment === 'dev') {
 
-        logger.add(new winston.transports.Console({
-            handleExceptions: true,
-            level: 'debug',
-        }));
-    }
+		logger.add(new winston.transports.Console({
+			handleExceptions: true,
+			level: 'debug',
+		}));
+	}
 
-    if (config.syslog) {
+	if (config.syslog) {
 
-        logger.add(
-            new Syslog(
-                {
-                    app_name: config.instanceName,
-                    handleExceptions: true,
-                    ...config.syslog
-                }
-            )
-        );
-    }
+		logger.add(
+			new Syslog(
+				{
+					app_name: config.instanceName,
+					handleExceptions: true,
+					...config.syslog
+				}
+			)
+		);
+	}
 
-    if (logger.transports.length === 0) {
-        throw new ApplicationError({
-            code: 'E_LOGGER_NO_TRANSPORT',
-            message: 'No transport is configured for the logger.'
-        });
-    }
+	if (logger.transports.length === 0) {
+		throw new ApplicationError({
+			code: 'E_LOGGER_NO_TRANSPORT',
+			message: 'No transport is configured for the logger.'
+		});
+	}
 
-    return logger;
+	return logger;
 };
